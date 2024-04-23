7b34e794cc8fbc440464a7531356a88476040812
3hreeman <santapemagic@gmail.com>
2024-04-23 17:42:55 +0900

    base script, scene added

---
+++ Boids/Assets/03Script/BoidsMain.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsMain : MonoBehaviour
+{
+    public GameObject boidPrefab;
+    public int numberOfBoids;
+    public List<GameObject> boids = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
