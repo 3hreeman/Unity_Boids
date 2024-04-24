a00717936abc3e7a53980377bf0b9b1f7be12426
3hreeman <santapemagic@gmail.com>
2024-04-24 23:38:45 +0900

    Layer mask added / base generate added

--- Boids/Assets/03Script/BoidObject.cs
+++ Boids/Assets/03Script/BoidObject.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoidObject : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class BoidObject : MonoBehaviour {
+    public float MoveSpd = 1.0f;
+    List<BoidObject> _nearBoidList = new List<BoidObject>();
+    public void Init(float spd) {
+        MoveSpd = spd;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+    
+    private void FindNearBoids() {
+        // Find near boids
+        _nearBoidList.Clear();
+        var colliders = Physics.OverlapSphere(this.transform.position, 5.0f, LayerMask.GetMask("BoidLayer"));
+        foreach (var collider in colliders) {
+            if (collider.gameObject != this.gameObject) {
+                var boid = collider.gameObject.GetComponent<BoidObject>();
+                if (boid != null) {
+                    _nearBoidList.Add(boid);
+                }
+            }
+        }
     }
 }
--- Boids/Assets/03Script/BoidsMain.cs
+++ Boids/Assets/03Script/BoidsMain.cs
@@ -7,11 +7,20 @@ public class BoidsMain : MonoBehaviour
     public GameObject boidPrefab;
     public int numberOfBoids;
     public List<GameObject> boids = new List<GameObject>();
-    
+    public float GenerateRadius = 10.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        CreateBoids();
+    }
+    
+    private void CreateBoids() {
+        for (int i = 0; i < numberOfBoids; i++) {
+            var pos = Random.insideUnitSphere * GenerateRadius;
+            var randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            GameObject boid = Instantiate(boidPrefab, pos, randomRotation);
+            boid.transform.parent = this.transform;
+            boids.Add(boid);
+        }
     }
 
     // Update is called once per frame
