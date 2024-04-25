6293f35cc05971ab362413322b408cd5206b526e
3hreeman <threeman@superplanet.net>
2024-04-25 18:56:19 +0900

    text mesh pro added / game pad added / boid object changed

---
+++ Boids/Assets/03Script/GamePadController.cs
@@ -0,0 +1,109 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.Combat {
+    public class GamePadController : MonoBehaviour {
+
+        [SerializeField] private RectTransform stick;
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private RectTransform padBgRect;
+        [SerializeField] private GameObject padObject;
+        [SerializeField, Range(10f, 150f)] private float stickRange;
+
+        public Vector2 inputVector;
+        private bool _isActive;
+        public bool isRun;
+
+        public Camera mainCam;
+
+        private void Awake() {
+            stickRange = padBgRect.sizeDelta.x / 2;
+            padObject.SetActive(false);
+        }
+
+        void ResetStick() {
+            stick.anchoredPosition = Vector2.zero;
+            inputVector = Vector2.zero;
+            isRun = false;
+        }
+        
+        void ControlStick(Vector2 pos) {
+            var inputDir = pos - rectTransform.anchoredPosition;
+            var clampDir = inputDir.magnitude < stickRange ? inputDir : inputDir.normalized * stickRange;
+            stick.anchoredPosition = clampDir;
+
+            inputVector = clampDir.normalized;
+            isRun = Mathf.Abs(inputVector.x) > 0.5f || Mathf.Abs(inputVector.y) > 0.5f ? true : false;
+            // playerUnit.UpdatePlayerInput(inputVector);
+        }
+        
+        public void OnEndDrag(PointerEventData eventData) {
+            if(_isActive == false) {
+                return;
+            }
+            Debug.LogWarning("OnEndDrag()");
+            stick.anchoredPosition = Vector2.zero;
+            _isActive = false;
+            isRun = false;
+        }
+
+        public Canvas canvas;
+
+        public void Update() {
+            UpdateControl();
+        }
+
+        private void UpdateControl() {
+            if (_isActive == false) {
+                if (Input.GetMouseButtonDown(0)) {
+                    SetControlState(true);
+                }
+            }
+            else {
+                if (Input.GetMouseButtonUp(0)) {
+                    SetControlState(false);
+                }
+            }
+
+
+            if (_isActive) {
+                ControlStick(Input.mousePosition / canvas.scaleFactor);
+            }
+        }
+
+        private void SetControlState(bool flag) {
+            if (_isActive == flag) {
+                return;
+            }
+            _isActive = flag;
+
+            if (_isActive) {
+                var inputPos = Input.mousePosition / canvas.scaleFactor;
+                rectTransform.anchoredPosition = inputPos;
+            }
+            else {
+                ResetStick();
+            }
+            padObject.SetActive(_isActive);
+        }
+        
+        private void EndControl() {
+            ResetStick();
+            padObject.SetActive(false);
+            _isActive = false;
+        }
+        
+        private bool IsPointerOverUIObject() {
+            PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+            eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+            return results.Count > 0;
+        }
+        
+        public string GetStatusText() {
+            return $"isInput: {_isActive}\ninputVector: {inputVector}\nTouchCount: {Input.touchCount}";
+        }
+    }
+}
