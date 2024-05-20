51830b43c06b2c6f05f2eda98c54049e135917bf
3hreeman <santapemagic@gmail.com>
2024-05-20 23:59:38 +0900

    job updater added

--- Boids/Assets/03Script/BoidsMain.cs
+++ Boids/Assets/03Script/BoidsMain.cs
@@ -1,39 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
-public class BoidsMain : MonoBehaviour
-{
+public class BoidsMain : MonoBehaviour {
     public static BoidsMain Instance;
-    public GameObject boidPrefab;
-    public int numberOfBoids;
-    public List<GameObject> boids = new List<GameObject>();
-    [Range(5, 20)] public float GenerateRadius = 10.0f;
-    [Range(1, 10)] public float BoidSpeed = 3.0f;
-    [Range(1, 10)] public float BoidRadius = 5.0f;
+
+    #region Variables & Initializer
+
+    [Header("Boid Options")] [SerializeField]
+    private BoidUnit boidUnitPrefab;
+
+    [Range(5, 5000)] public int boidCount;
+    [Range(10, 100)] public float spawnRange = 30;
+    public Vector2 speedRange;
     
-    public float CohesionWeight = 1;
-    public float SeparationWeight = 1;
-    public float AlignmentWeight = 1;
-    
-    public int MaxNearBoid = 10;
-    public static float GlobalSpd => Instance.BoidSpeed;
-    public static float GlobalRadius => Instance.BoidRadius
-    ;
-    // Start is called before the first frame update
+    [Range(0, 10)] public float cohesionWeight = 1;
+    [Range(0, 10)] public float alignmentWeight = 1;
+    [Range(0, 10)] public float separationWeight = 1;
+
+    [Range(0, 100)] public float boundsWeight = 1;
+    [Range(0, 100)] public float obstacleWeight = 10;
+    [Range(0, 10)] public float egoWeight = 1;
+
+
+    public BoidUnit currUnit;
+    private MeshRenderer boundMR;
+    [SerializeField] LayerMask unitLayer;
+
+    [Header("Camera")] [SerializeField] CinemachineVirtualCamera originCam;
+    [SerializeField] CinemachineFreeLook unitCam;
+
+    [Header("OPTIONAL")] public bool ShowBounds = false;
+    public bool cameraFollowUnit = false;
+    public bool randomColor = false;
+    public bool blackAndWhite = false;
+    public bool protectiveColor = false;
+    public float enemyPercentage = 0.01f;
+    public Color[] GizmoColors;
+
     void Start() {
-        Instance = this;
-        CreateBoids();
-    }
-    
-    private void CreateBoids() {
-        for (int i = 0; i < numberOfBoids; i++) {
-            var pos = Random.insideUnitSphere * GenerateRadius;
-            var randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
-            GameObject boid = Instantiate(boidPrefab, pos, randomRotation);
-            boid.transform.parent = this.transform;
-            boid.GetComponent<BoidObject>().Init(BoidSpeed);
-            boids.Add(boid);
+        // Generate Boids
+        boundMR = GetComponentInChildren<MeshRenderer>();
+        for (int i = 0; i < boidCount; i++) {
+            Vector3 randomVec = Random.insideUnitSphere;
+            randomVec *= spawnRange;
+            Quaternion randomRot = Quaternion.Euler(0, Random.Range(0, 360f), 0);
+            BoidUnit currUnit = Instantiate(boidUnitPrefab, this.transform.position + randomVec, randomRot);
+            currUnit.transform.SetParent(this.transform);
+            currUnit.InitializeUnit(this, Random.Range(speedRange.x, speedRange.y), i);
         }
+    }
+
+    #endregion
+
+    void Update() {
+        // Right Mouse button Down, Set the unit to currUnit
+        if (Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 1000, Color.red, 3f);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, unitLayer)) {
+                currUnit = hit.transform.GetComponent<BoidUnit>();
+            }
+            else {
+                currUnit = null;
+            }
+        }
+
+        // we can see curr Unit's neighbour & target Vector
+        if (currUnit) {
+            currUnit.DrawVectorGizmo(0);
+        }
+
+        // camera switch
+        if (cameraFollowUnit && currUnit != null) {
+            originCam.gameObject.SetActive(false);
+            unitCam.gameObject.SetActive(true);
+            unitCam.Follow = currUnit.transform;
+            unitCam.LookAt = currUnit.transform;
+        }
+        else {
+            originCam.gameObject.SetActive(true);
+            unitCam.gameObject.SetActive(false);
+        }
+
+        // can see bounds
+        boundMR.enabled = ShowBounds;
     }
 }
