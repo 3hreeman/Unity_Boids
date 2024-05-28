29426999713a9408df1c3f981b03bc0b903b334b
3hreeman <threeman@superplanet.net>
2024-05-28 19:00:37 +0900

    something changed

--- Boids/Assets/03Script/Test/Dots/DotsTestSystem.cs
+++ Boids/Assets/03Script/Test/Dots/DotsTestSystem.cs
@@ -2,6 +2,7 @@ using Unity.Burst;
 using Unity.Entities;
 
 public partial struct DotsTestSystem : ISystem {
+    //TestMain에서 실행할 내용에 대한 구현이 들어가야함
     
     [BurstCompile]
     public void OnCreate(ref SystemState state) {
