62527c4bb6925a2207f580702a1b4c1cce25927c
3hreeman <threeman@superplanet.net>
2024-05-28 18:42:48 +0900

    entity system tests scene added

---
+++ Boids/Assets/03Script/Test/Dots/DotsTestSystem.cs
@@ -0,0 +1,15 @@
+using Unity.Burst;
+using Unity.Entities;
+
+public partial struct DotsTestSystem : ISystem {
+    
+    [BurstCompile]
+    public void OnCreate(ref SystemState state) {
+        
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) {
+        
+    }
+}
