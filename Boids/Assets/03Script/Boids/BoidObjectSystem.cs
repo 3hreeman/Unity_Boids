fe5f6f2412aab9ff5dc6a90d659cc20ce10afd56
3hreeman <santapemagic@gmail.com>
2024-06-03 15:40:41 +0900

    boid dots move test / trail added (but not available)

--- Boids/Assets/03Script/Boids/BoidObjectSystem.cs
+++ Boids/Assets/03Script/Boids/BoidObjectSystem.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 public partial struct BoidObjectSystem : ISystem {
     
     
     public void OnUpdate(ref SystemState state) {
+        var tick = (float)SystemAPI.Time.DeltaTime;
+        foreach (var (boid, xform) in SystemAPI.Query<RefRO<BoidObject>, RefRW<LocalTransform>>()) {
+            xform.ValueRW.Position += boid.ValueRO.TargetVector * boid.ValueRO.Speed * tick;
+            xform.ValueRW.Rotation = Quaternion.LookRotation(boid.ValueRO.TargetVector);
+        }
     }
 }
