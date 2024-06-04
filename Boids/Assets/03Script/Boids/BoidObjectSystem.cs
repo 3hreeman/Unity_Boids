4c1bac26815ff33f215ec210977ac11c7656b89d
3hreeman <santapemagic@gmail.com>
2024-06-04 22:41:39 +0900

    boid move system described

--- Boids/Assets/03Script/Boids/BoidObjectSystem.cs
+++ Boids/Assets/03Script/Boids/BoidObjectSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -9,15 +10,21 @@ using Random = Unity.Mathematics.Random;
 [UpdateInGroup(typeof(SimulationSystemGroup))]
 public partial struct BoidObjectSystem : ISystem {
 
+    [BurstCompile]
     public void OnUpdate(ref SystemState state) {
         var tick = (float)SystemAPI.Time.DeltaTime;
 
         foreach (var (boid, xform) in SystemAPI.Query<RefRW<BoidObject>, RefRW<LocalTransform>>()) {
             // CalculateVectors(boid, xform);
-            boid.ValueRW.TargetVector = CalculateBoundsVector(boid, xform);
-            boid.ValueRW.EgoVector = UnityEngine.Random.insideUnitSphere;
+            var targetVector = CalculateVectors(boid, xform);
+            targetVector = Vector3.Lerp(xform.ValueRO.Forward(), targetVector, tick);
+            targetVector = targetVector.normalized;
+            if(targetVector == Vector3.zero) {
+                targetVector = boid.ValueRO.EgoVector;
+            }
+            boid.ValueRW.TargetVector = targetVector;
             xform.ValueRW.Position += (boid.ValueRO.TargetVector + boid.ValueRW.EgoVector)* boid.ValueRO.Speed * tick;
-            // xform.ValueRW.Rotation = Quaternion.LookRotation(boid.ValueRO.TargetVector);
+            xform.ValueRW.Rotation = Quaternion.LookRotation(boid.ValueRO.TargetVector);
         }
     }
 
@@ -27,12 +34,28 @@ public partial struct BoidObjectSystem : ISystem {
         
     }
     
-    public void CalculateVectors(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
+    public Vector3 CalculateVectors(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
         var boundsVector = CalculateBoundsVector(boid, xform);
-        boid.ValueRW.TargetVector = boundsVector;
+        var cohesionVector = CalculateCohesionVector(boid, xform);
+        var alignmentVector = CalculateAlignmentVector(boid, xform);
+        var separationVector = CalculateSeparationVector(boid, xform);
+        var targetVector = cohesionVector + alignmentVector + separationVector + boundsVector + boid.ValueRW.EgoVector;
+        return targetVector;
     }
     
-    private Vector3 CalculateBoundsVector(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
+    public float3 CalculateCohesionVector(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
+        return Vector3.zero;
+    }
+    
+    public float3 CalculateAlignmentVector(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
+        return Vector3.zero;
+    }
+    
+    public float3 CalculateSeparationVector(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
+        return Vector3.zero;
+    }
+    
+    private float3 CalculateBoundsVector(RefRW<BoidObject> boid, RefRW<LocalTransform> xform) {
         Vector3 offsetToCenter= new float3(0, 50, 0) - xform.ValueRW.Position;
         return offsetToCenter.magnitude >= 30 ? offsetToCenter.normalized : Vector3.zero;
     }
