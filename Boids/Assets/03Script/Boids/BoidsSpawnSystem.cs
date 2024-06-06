77c241941117cb9efd8a201c9864d238e874c754
3hreeman <santapemagic@gmail.com>
2024-06-07 00:18:50 +0900

    BoidConfig is no more singleton / two separated boids system operationg

--- Boids/Assets/03Script/Boids/BoidsSpawnSystem.cs
+++ Boids/Assets/03Script/Boids/BoidsSpawnSystem.cs
@@ -15,9 +15,33 @@ public partial struct BoidsSpawnSystem : ISystem {
     }
     
     public void OnUpdate(ref SystemState state) {
+
+        foreach(var config in SystemAPI.Query<RefRO<BoidConfig>>()) {
+            var instances = state.EntityManager.Instantiate(config.ValueRO.Prefab, config.ValueRO.Count, Allocator.Temp);
+            foreach (var entity in instances) {
+                var tr = SystemAPI.GetComponentRW<LocalTransform>(entity);
+                var obj = SystemAPI.GetComponentRW<BoidObject>(entity);
+                var ego = SystemAPI.GetComponentRW<BoidEgo>(entity);
+                obj.ValueRW.BasePosition = config.ValueRO.BasePosition;
+                obj.ValueRW.BoundRange = config.ValueRO.SpawnRange;
+                obj.ValueRW.CenterOffset = config.ValueRO.CenterOffset;
+                
+                float3 randomVec = Random.insideUnitSphere;
+                randomVec *= config.ValueRO.SpawnRange;
+                tr.ValueRW.Position = config.ValueRO.BasePosition + randomVec; 
+                Quaternion randomRot = Quaternion.Euler(0, Random.Range(0, 360f), 0);
+                tr.ValueRW.Rotation = randomRot;
+
+                obj.ValueRW.Speed = Random.Range(config.ValueRO.SpeedRange.x, config.ValueRO.SpeedRange.y);
+                obj.ValueRW.TargetVector = new float3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                ego.ValueRW.EgoVector = new float3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            }
+        }
+        
+        /*
         var config = SystemAPI.GetSingleton<BoidConfig>();
         var instances = state.EntityManager.Instantiate(config.Prefab, config.Count, Allocator.Temp);
-        
+
         foreach (var entity in instances) {
             var tr = SystemAPI.GetComponentRW<LocalTransform>(entity);
             var obj = SystemAPI.GetComponentRW<BoidObject>(entity);
@@ -33,6 +57,7 @@ public partial struct BoidsSpawnSystem : ISystem {
             obj.ValueRW.TargetVector = new float3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             ego.ValueRW.EgoVector = new float3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         }
+        */
 
         state.Enabled = false;      //한번의 플로우가 끝나면 해당 시스템을 비활성화
     }
